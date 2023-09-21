b9c52f5f75f0c1fa621c83d5af688b4d9e74b01b
alejandro cardoso marcolino mendonça rocha <alejandrowork2003@gmail.com>
2023-09-21 10:58:15 -0300

    0.007.1 fixed one bug with ABOUT display

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,37 +1,45 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main class="flex justify-around ">
-    <img src="../assets/WELCOME.jpg" alt="nothing here" class="w-5/12 h-2/5 border-2 border-blue-600">
+  <main class="flex justify-around">
+    <img
+      src="../assets/WELCOME.jpg"
+      alt="nothing here"
+      class="w-5/12 h-2/5 border-2 border-blue-600"
+    />
 
-    <div class="border-2 border-black w-5/12 msg bg-[url('../assets/backgroundAbout.jpg')] ">
+    <div class="border-2 border-black w-5/12 msg bg-[url('../assets/backgroundAbout.jpg')]">
+      <div class="text-7xl aboutTitle w-full h-3 my-8 flex items-center justify-center">
+        Hello my friends
+      </div>
 
-      <div class="text-7xl aboutTitle w-full h-3 my-8  flex items-center justify-center"> Hello my friends</div>
-     
-      <div class="about w-full h-auto  flex items-center justify-center">We are one Web-Blog that exists just on purpose of have some posts and some use educational and some ways to learn and practice some tools</div>
-
+      <p class="about w-full h-auto flex items-center justify-center p">
+        We are one Web-Blog that exists just on purpose of have some posts and some use educational
+        and some ways to learn and practice some tools
+      </p>
     </div>
   </main>
-
 </template>
 
-<style>
-.aboutTitle{
+<style scoped>
+.p {
+  line-height: 70px;
+}
+.aboutTitle {
   font-family: 'Dancing Script', cursive;
   font-weight: bolder;
   padding: 3rem;
 }
- 
-.about{
+
+.about {
   font-size: 50px;
   font-family: 'Dancing Script', cursive;
   font-weight: bolder;
   padding: 2rem;
-} 
+}
 
-.msg{
+.msg {
   background-image: url(../assets/backgroundAbout.jpg);
   background-size: 100%;
-};
+}
 </style>
